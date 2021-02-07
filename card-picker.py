f2f343adcfeaafdaecefc9ebdabdb7b88231fd7b
hwhuang27 <hwhuang@sfu.ca>
2021-02-06 20:18:20 -0800

    added class card_info and special case handling

--- card-picker.py
+++ card-picker.py
@@ -1,31 +1,52 @@
-import sys
+import sys, string, requests
 import numpy as np
 import pandas as pd
-import requests
+import tkinter as tk
 from pprint import pprint
 from bs4 import BeautifulSoup
-import tkinter as tk
 
 class card_info():
-
 	def __init__(self, soup):
-		self.labels = np.empty(7, dtype='object')
-		self.values = np.empty(7, dtype='object')
+		self.labels = np.chararray(7, itemsize=512, unicode=True)
+		self.values = np.chararray(7, itemsize=512, unicode=True)
 
 		# extract card labels from the HTML on the website
 		card_labels = soup.find_all('h3', class_="pi-data-label pi-secondary-font")
 		for index, label in enumerate(card_labels):
+			#print(str(label.text))
 			self.labels[index] = str(label.text)
-			print(self.labels[index])
+			#print(self.labels[index])
 
 		# extract corresponding card values
 		card_data = soup.find_all('div', class_="pi-data-value pi-font")
 		for index, value in enumerate(card_data):
 			self.values[index] = str(value.text)
-			print(self.values[index])
+			#print(self.values[index])
+
+
+		# extract card picture
+
+		# extract card title
+
+
+	def print_labels(self):
+		print(self.labels)
+
+	def print_values(self):
+		print(self.values)
+
 
 def main():
 	card_name = input("Enter card name: ")
+
+	# capitalize if not already and handle special cases
+	if "'" in card_name:
+		card_name = card_name.replace("'", "%27")
+	card_name = string.capwords(card_name)
+	if " " in card_name:
+		card_name = card_name.replace(" ", "_")
+
+	# strike/defend defaults to class: defect 
 	if card_name == 'Strike':
 		card_name = 'Strike_(Defect)'
 	if card_name == 'Defend':
@@ -36,9 +57,13 @@ def main():
 	card_soup = BeautifulSoup(page.content, 'lxml')
 
 	card = card_info(card_soup)
-	print(card.labels)
-	print(card.values)
+	if card.labels[0] == '':
+		print('Card not found. Maybe check your spelling?')
+		sys.exit(0)
 
+	
+	card.print_labels()
+	card.print_values()
 
 
 
